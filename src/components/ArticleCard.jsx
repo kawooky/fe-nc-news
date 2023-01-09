44ce22fab0440aea1eca51bc37778c43ef88b451
kawooky <kawooky@gmail.com>
2023-01-09 16:26:11 +0000

    updated css

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -7,11 +7,11 @@ export const ArticleCard = ({article_id, title, topic, author, body, created_at,
             <Link to={`/${article_id}`}>
             <h2>{title}</h2>
             </Link>
-            <h3>Topic:{topic}</h3>
-            <h3>Author:{author}</h3>
+            <h3>Topic: {topic}</h3>
+            <h3>Author: {author}</h3>
             <h4>Body: {body}</h4>
-            <p>Created At:{created_at}</p>
-            <p>Votes:{votes}</p>
+            <p>Created At: {created_at}</p>
+            <p>Votes: {votes}</p>
             <p>Comment Count: {comment_count}</p>
         </article>
         
