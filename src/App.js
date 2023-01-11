f15303d602ca190ffc72404b083738f8bd1fb77b
kawooky <kawooky@gmail.com>
2023-01-11 12:03:29 +0000

    new comment box added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import {Header} from "./components/Header";
 import { Route, Routes } from "react-router-dom";
 import {ArticleList} from "./components/ArticleList";
 import { SingleArticle } from './components/SingleArticle';
+import { useState } from 'react';
 
 function App() {
+  const [username, setUsername] =useState('')
 
 
   return (
     <div className="App">
-      <Header />
+      <Header setUsername={setUsername}/>
       <Routes>
         <Route
           path="/"
           element={<ArticleList />}
         ></Route>
-        <Route path="/articles/:articleId" element={<SingleArticle />}></Route>
+        <Route path="/articles/:articleId" element={<SingleArticle username={username}/>}></Route>
       </Routes>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
+import { useEffect , useState} from "react";
 import { Link } from "react-router-dom";
+import { getUsers } from "../api";
 
-export const Header = () => {
+export const Header = ({setUsername}) => {
+        const[allUsernames, setAllUsernames] =useState([])
+
+        useEffect(() => {
+        getUsers()
+        .then((users) => {
+            const usersUsernames = users.map((user) => {
+                return user.username
+            })
+            setAllUsernames(usersUsernames)
+        })
+    } , [])
   return (
-  <Link to="/">
-  <h1 className="app-header">NC-NEWS</h1>
-  </Link>
+    <div>
+    <Link to="/">
+    <h1 className="app-header">NC-NEWS</h1>
+    </Link>
+
+    <select name="select-username" id="select-username" onChange={(event) => setUsername(event.target.value)}>
+        <option value='' default>Select</option>
+        {allUsernames.map((username)=> {
+            return (
+                <option value={username} key={username}>{username}</option>)})}
+    </select>
+
+    </div>
   )
 };
